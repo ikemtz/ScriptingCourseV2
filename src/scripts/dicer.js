d08c0f60591d9f9426b5c1970de647938b9c0219
Isaac Martinez <Ikemtz@outlook.com>
2018-12-09 16:17:58 -0500

    Adding range control

--- src/scripts/dicer.js
+++ src/scripts/dicer.js
@@ -1,9 +1,41 @@
 'Strict Mode';
 let currentDiceValues = {};
 let rollTemplate = '';
-let rollsElm = {};
+let speed = 50;
+let interval;
+const speedElm = document.getElementById('speed');
+const die1Elm = document.getElementById('die1');
+const die2Elm = document.getElementById('die2');
+const rollsElm = document.getElementById('rolls');
+const speedRangeElm = document.getElementById('speedRange');
 let isEmpty;
 
+function speedUp() {
+  if (speed <= 10) {
+    updateRoller(1);
+  } else if (0 < speed % 10) {
+    updateRoller(speed - (speed % 10));
+  } else {
+    updateRoller(speed - 10);
+  }
+}
+
+function slowDown() {
+  if (speed >= 90) {
+    updateRoller(99);
+  } else if (0 < speed % 10) {
+    updateRoller(speed + ((10 - speed) % 10));
+  } else {
+    updateRoller(speed + 10);
+  }
+}
+
+function updateRoller(updatedSpeed) {
+  speed = updatedSpeed;
+  speedRangeElm.value = speed;
+  clearInterval(interval);
+  rollTheDice();
+}
 // Takes the current dice and add it's to the DOM.
 function captureRoll() {
   if (isEmpty) {
@@ -25,21 +57,28 @@ function clearRolls() {
   isEmpty = true;
 }
 
-//Initializes the randomization of the die
-(() => {
+function rollTheDice() {
+  // Random Function
   const ran = () => Math.floor(Math.random() * 6) + 1;
-  const die1Elm = document.getElementById('die1');
-  const die2Elm = document.getElementById('die2');
-  rollsElm = document.getElementById('rolls');
-  //In the markup I had to change "img src" to "img [src]" to avoid having the browser load invalid images on load
-  rollTemplate = rollsElm.innerHTML.replace(/\[src\]/gim, 'src');
-  clearRolls();
-  setInterval(() => {
+
+  speedElm.innerText = 100 - speed;
+  return (interval = setInterval(() => {
     currentDiceValues = {
       die1: `./assets/images/dice${ran()}.png`,
       die2: `./assets/images/dice${ran()}.png`
     };
     die1Elm.src = currentDiceValues.die1;
     die2Elm.src = currentDiceValues.die2;
-  }, 500);
+  }, speed * 10));
+}
+
+//Initializes the randomization of the die
+(() => {
+  /*
+    In the markup I had to change "img src" to "img [src]" to avoid having the 
+    browser load invalid images on load
+  */
+  rollTemplate = rollsElm.innerHTML.replace(/\[src\]/gim, 'src');
+  clearRolls();
+  rollTheDice();
 })();
