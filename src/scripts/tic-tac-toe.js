2b3c850ed9faabe3799f543635c898e9e5c5e159
Isaac Martinez <Ikemtz@outlook.com>
2018-12-09 21:44:10 -0500

    Adding Tic-Tac-Toe

---
+++ src/scripts/tic-tac-toe.js
@@ -0,0 +1,102 @@
+'Strict Mode';
+const xElement = '<div>X</div>';
+const oElement = '<div>O</div>';
+const rowArray = ['a', 'b', 'c'];
+const colArray = [0, 1, 2];
+const diag1Array = ['a0', 'b1', 'c2'];
+const diag2Array = ['a2', 'b1', 'c0'];
+let gameFinished = false;
+const board = {
+  a0: null,
+  a1: null,
+  a2: null,
+  b0: null,
+  b1: null,
+  b2: null,
+  c0: null,
+  c1: null,
+  c2: null
+};
+
+let x = false;
+
+function drawBoard() {
+  if (!(gameFinished = checkForWinner())) {
+    x = !x;
+    document.getElementById('currentPlayer').innerText = x ? "X" : "O";
+  }
+  else {
+    alert(`Player ${x ? "X" : "O"} won!`);
+  }
+  for (var item in board) {
+    document.getElementById(item).innerHTML = board[item] === null ? null : board[item] ? xElement : oElement;
+  }
+}
+
+function checkForWinner() {
+  //check for matching rows;
+  var matchingRows = rowArray.filter(row =>
+    colArray.map(col => board[`${row}${col}`])
+      .every(matchLogic));
+  if (matchingRows.length > 0) {
+    colArray.forEach(col => document.getElementById(`${matchingRows[0]}${col}`).classList.add('winningTile')
+    );
+    return true;
+  }
+
+  //check for matching cols;
+  var matchingCols = [0, 1, 2].filter(col =>
+    rowArray.map(row => board[`${row}${col}`])
+      .every(matchLogic));
+  if (matchingCols.length > 0) {
+    rowArray.forEach(row => document.getElementById(`${row}${matchingCols[0]}`).classList.add('winningTile')
+    );
+    return true;
+  }
+
+  //check for diag1;
+  var hasMatch = diag1Array.map(item => board[item]).every(matchLogic);
+  if (hasMatch) {
+    diag1Array.forEach(item => document.getElementById(`${item}`).classList.add('winningTile')
+    );
+    return true;
+  }
+
+  //check for diag2
+  hasMatch = diag2Array.map(item => board[item]).every(matchLogic);
+  if (hasMatch) {
+    diag2Array.forEach(item => document.getElementById(`${item}`).classList.add('winningTile')
+    );
+    return true;
+  }
+}
+
+function matchLogic(value, index, array) {
+  return value !== null && value === array[0];
+}
+
+function resetGame() {
+  for (var item in board) {
+    board[item] = null;
+    document.getElementById(`${item}`).classList.remove('winningTile');
+  }
+  x = !x;
+  drawBoard();
+}
+
+function tileClicked(tile) {
+  if (board[tile] !== null) {
+    alert('That move is not allowed!')
+  }
+  else if (gameFinished) {
+    alert('The current game has finished, no more moves are allowed!');
+  }
+  else {
+    board[tile] = x;
+    drawBoard();
+  }
+}
+//initializes the board
+(() => {
+  drawBoard();
+})();
